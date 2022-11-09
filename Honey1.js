449f0e79698cf45f36189cb41572fa396aa2d934
obliviosa <91109032+obliviosa@users.noreply.github.com>
2022-11-09 13:45:37 +1100

    Add files via upload

---
+++ Honey1.js
@@ -0,0 +1,304 @@
+var prompts = [
+    {
+        prompt: '<img src="assets/NA12stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA12stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA12spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA12spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    },
+    {
+        prompt: '<img src="assets/NA13stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA13stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA13spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA13spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    },
+    {
+        prompt: '<img src="assets/NA14stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA14stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA14spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA14spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    },
+    {
+        prompt: '<img src="assets/NA23stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA23stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA23spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA23spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    },
+    {
+        prompt: '<img src="assets/NA24stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA24stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA24spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA24spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    },
+    {
+        prompt: '<img src="assets/NA34stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA34stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA34spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA34spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    },
+    {
+        prompt: '<img src="assets/NA56stackleft.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA56stackright.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA56spreadleft.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA56spreadright.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA57stackleft.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA57stackright.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA57spreadleft.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA57spreadright.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA58stackleft.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA58stackright.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA58spreadleft.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA58spreadright.png">',
+        a: ['bait_N', 'stack_W', 'stack_0']
+    },
+    {
+        prompt: '<img src="assets/NA67stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA67stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA67spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA67spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    },
+    {
+        prompt: '<img src="assets/NA68stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA68stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA68spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA68spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    },
+    {
+        prompt: '<img src="assets/NA78stackleft.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA78stackright.png">',
+        a: ['bait_D', 'stack_S', 'spread_5']
+    },
+    {
+        prompt: '<img src="assets/NA78spreadleft.png">',
+        a: ['bait_D', 'clock_NW', 'stack_L']
+    },
+    {
+        prompt: '<img src="assets/NA78spreadright.png">',
+        a: ['bait_D', 'clock_NW', 'stack_R']
+    }
+]
+
+var ANSWERS = ['bait_N', 'bait_D', 'stack_W', 'stack_S', 'clock_SW','clock_N','clock_SE', 'clock_W',
+               'clock_E', 'clock_SW','clock_S','clock_SE', 'stack_L', 'stack_R', 'stack_0', 'spread_1',
+               'spread_2', 'spread_3', 'spread_4', 'spread_5', 'spread_6', 'spread_7', 'spread_8'];
+var answer = [];
+var inputAnswer = [];
+var numCorrect = 0; // yeah yeah this naming is confusing
+var correct = false;
+var lastPrompt = 0;
+var timeLimit = 0;
+var timeoutEnabled = false;
+var timeoutHandle;
+// var wrong = 0;
+
+let prompt = document.getElementById('prompt');
+let n = Math.floor(Math.random() * prompts.length);
+prompt.innerHTML = prompts[n].prompt;
+answer = prompts[n].a;
+
+var nextButton = document.getElementById('next');
+nextButton.onclick = () => {
+    correct = false;
+    startTimerIfEnabled();
+
+    let prompt = document.getElementById('prompt');
+
+    prompt.className = 'd-inline';
+    ANSWERS.forEach((a) => {
+        console.log(a);
+        let answerButton = document.getElementById(a);
+        answerButton.className = 'btn btn-primary';
+    });
+
+    let n = 1 + Math.floor(Math.random() * (prompts.length-1));
+    n = (lastPrompt + n) % prompts.length;
+    lastPrompt = n;
+    prompt.innerHTML = prompts[n].prompt;
+    answer = prompts[n].a;
+    inputAnswer = [];
+}
+
+var answerButtons = document.getElementById('answerButtons');
+answerButtons.onclick = (e) => {
+    if(e.target.id === 'answerButtons') {return;}
+    console.log(e.target.id);
+    let a = e.target.id;
+
+    if(a === answer[inputAnswer.length]) {
+        document.getElementById(a).className = 'btn btn-success';
+        inputAnswer.push(a);
+
+        ANSWERS.forEach((a) => {
+            let answerButton = document.getElementById(a);
+            a.className = 'btn btn-primary';
+        });
+
+        if(inputAnswer.length >= 3) {
+            let prompt = document.getElementById('prompt');
+            prompt.className = 'd-inline bg-success';
+
+            numCorrect+=1
+            correct = true;
+        }
+    } else {
+        e.target.className = 'btn btn-danger';
+        numCorrect = 0;
+    }
+    let streak = document.getElementById('streak');
+    streak.innerText = `Streak: ${numCorrect}`;
+}
+
+function timerEnableToggled(checkbox)
+{
+    if(checkbox.checked == true) {
+        document.getElementById("timeLimitInput").removeAttribute("disabled");
+        timeoutEnabled = true;
+        startTimerIfEnabled();
+    } else {
+        document.getElementById("timeLimitInput").setAttribute("disabled", "disabled");
+        timeoutEnabled = false;
+        resetTimer();
+   }
+}
+
+function startTimerIfEnabled() {
+    // Reset timer so that users don't fail twice
+    resetTimer();
+
+    if (timeoutEnabled) {
+        timeLimit = parseInt(document.getElementById('timeLimitInput').value);
+        timeoutHandle = setTimeout(failure, timeLimit * 1000);
+    }
+}
+
+function resetTimer() {
+    if (timeoutHandle != null) {
+        clearTimeout(timeoutHandle);
+        timeoutHandle = null;
+    }
+}
+
+function failure() {
+    if (correct === false) {
+        numCorrect = 0;
+        let streak = document.getElementById('streak');
+        streak.innerText = `Streak: ${numCorrect}`;
+
+        alert("Time out - You died");
+    }
+}
